df7bf9ddb9d63a7c3ce2d0533b9e500421c0cb1e
asamarco <asamarco@users.noreply.github.com>
2026-01-24 16:45:41 +0100

    Allow all sources for img-src in CSP
    Allows to use any URL to upload images in the node form

--- src/middleware.ts
+++ src/middleware.ts
@@ -33,7 +33,7 @@ export function middleware(request: NextRequest) {
     `default-src 'self'`,
     `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
     `font-src 'self' https://fonts.gstatic.com`,
-    `img-src 'self' data:`,
+    `img-src 'self' data: *`,
     `script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDevelopment ? "'unsafe-eval'" : ""}`,
     `object-src 'none'`,
     `base-uri 'self'`,
